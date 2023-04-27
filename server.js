2411c0a95c3f27d39d0d32d42b4359b53851c04f
Jake Baek <jake.baek@soulfx.com>
2023-04-26 20:10:12 -0400

    fix: applied mongo db | installed library

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
+const uri = "mongodb+srv://jakejonggubaek:Cdspace1!@cluster0.c8yeb4l.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(8080, ()=>{
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+  } finally {
+    await client.close();
+  }
+}
+
+const db = client.db('Mern-to-do');
+db.collection('post').insertOne({name: 'jake', look: 'hot'}, (err, res) => {
+  console.log('data stored');
+})
+
+app.listen('8080', ()=>{
+  console.log('website opened');
 });
 
 app.get('/write', (req, res)=>{
@@ -16,3 +44,5 @@ app.get('/', (req, res)=>{
 app.post('/add', (req, res)=>{
   res.send('success')
 })
+
+run().catch(console.dir);
