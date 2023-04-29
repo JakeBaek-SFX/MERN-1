ac7341b12a53102b7786bb96ecf023587072234d
Jake Baek <jake.baek@soulfx.com>
2023-04-28 22:23:20 -0400

    fix: added write html

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 const app = express();
 const uri = "mongodb+srv://jakejonggubaek:Cdspace1!@cluster0.c8yeb4l.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+app.set('View engine', 'ejs');
+
+var db;
+MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, client) {
+  if (err) return console.log(err)
+
+  app.listen(8080, function () {
+    console.log('listening on 8080')
+  });
+
+  app.get('/write', (req, res)=>{
+    res.sendFile(__dirname + '/write.html')
+  });
+  
+  app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+  })
+  
+  app.post('/add', (req, res) => {
+    db = client.db('Mern-to-do');
+    db.collection('post').insertOne({toDo : req.body.toDo, due : req.body.dueDate} , function(err, res){
+      console.log('SUCCESS');
+      res.send({ status: 'SUCCESS' });
+    });
+  });
+  
+  app.get('/list', (req, res)=>{
+    db = client.db('Mern-to-do');
+    db.collection('post').find().toArray((err, data)=>{
+      console.log('res', data);
+      res.render('list.ejs', {results: data});
+    });
     
-  } finally {
-    await client.close();
-  }
-}
-
-const db = client.db('Mern-to-do');
-db.collection('post').insertOne({name: 'jake', look: 'hot'}, (err, res) => {
-  console.log('data stored');
-})
-
-app.listen('8080', ()=>{
-  console.log('website opened');
+  });
 });
-
-app.get('/write', (req, res)=>{
-  res.sendFile(__dirname + '/write.html')
-});
-
-app.get('/', (req, res)=>{
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/add', (req, res)=>{
-  res.send('success')
-})
-
-run().catch(console.dir);
